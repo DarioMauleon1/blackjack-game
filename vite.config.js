dcf7d040947c2f947bdfbb0927a5f4b7e784eb48
Dario Mauleon <dario.mauleon@gmail.com>
2024-12-30 13:32:14 -0600

    vite.config update

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+export default defineConfig({
+  base: "/blackjack-game/",
+});
+
+// Código creado para que gitpages funciones ya que al tomar como sub directorio /blackjack-game/ no encontraba la ruta del css y js que originalemtne en nuestro código están en el root y no en un sub directorio como creo gitpages
